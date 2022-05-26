c14a933837201edd5a9393f00be70d183bba42bd
quantumbeep <kh@khs-mac-mini.lan>
2022-05-26 15:57:09 -0700

    all tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, numRepeat) {
+  let result = '';
+  for (let i = 0; i < numRepeat; i++) {
+    result = `${result}${str}`;
+  }
+  if (numRepeat > -1) {
+    return result;
+  } else {
+    return 'ERROR';
+  }
 };
 
 // Do not edit below this line
