c339a9221ecd91272a046c2f52a75a53816ae5ca
quantumbeep <kh@khs-mac-mini.lan>
2022-05-27 14:30:48 -0700

    all tests passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 400 === 0) {
+    return true;
+  } else if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+//Leap Year
+//is it divisible by 4?
+//is it divisible by 400?
+
+//NOT Leap Year
+//is it divisible by 100?
