0c942e553013bd373fcff81af786ecef998c0b68
quantumbeep <kh@khs-mac-mini.lan>
2022-06-07 11:28:45 -0700

    test passed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function (objArray) {
+  //accepts an array of objects as argument
+  //returns an array
 
+  let result = objArray.map((object) => object.title);
+
+  console.log(result);
+  return result;
 };
 
 // Do not edit below this line
