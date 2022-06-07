b32176442b476c956f3ccf8c53f6187492cb6af5
quantumbeep <kh@khs-mac-mini.lan>
2022-06-06 17:14:26 -0700

    all tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (args) {
+  const result = args.reduce((prev, curr) => prev + curr, 0);
+  return result;
+  // for(let i = 0; i <= args.length; i++ ) {
+  //   args += args[i]
+  // }
+  // return args
 };
 
-const multiply = function() {
-
+const multiply = function (args) {
+  const result = args.reduce((prev, curr) => prev * curr);
+  return result;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num > 0) {
+    let newArray = [];
+    for (let i = 1; i <= num; i++) {
+      newArray.push(i);
+    }
+    console.log(newArray);
+    const result = newArray.reduce((prev, curr) => prev * curr);
+    return result;
+  } else {
+    return 1;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +45,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
