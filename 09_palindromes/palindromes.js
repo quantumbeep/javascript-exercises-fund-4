c15593831050bca089a9dd35751c589815535f15
quantumbeep <kh@khs-mac-mini.lan>
2022-06-06 18:38:55 -0700

    all tests passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const pattern = /[A-Za-z]/g;
+  const str1 = str.match(pattern).join('').toLowerCase();
+  console.log(str1);
 
+  const str2 = str.match(pattern).reverse().join('').toLowerCase();
+  console.log(str2);
+
+  if (str1 === str2) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
+
 module.exports = palindromes;
