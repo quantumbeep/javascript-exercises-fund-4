2ff7fbeb1a3804ffcfb63e37e3bcfc15aef04049
quantumbeep <kh@khs-mac-mini.lan>
2022-06-07 10:55:27 -0700

    all tests passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    if(num > 0 ){
+        let array = [0, 1];
+        let position0;
+        let position1;
+        for (let i = 1; i < num; i++) {
+          position0 = array[0];
+          position1 = array[1];
+          array[1] = position0 + position1;
+          array[0] = position1;
+          console.log(array);
+        }
+        return array[1];
+    } else {
+        return "OOPS"
+    }
 };
 
 // Do not edit below this line
